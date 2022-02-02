050a73703dcb344503301e72492c01d01c23c195
keyzen <alonso.anavalon@gmail.com>
2022-02-02 19:27:52 -0300

    firsttestinstruments

--- src/routes/getData.js
+++ src/routes/getData.js
@@ -1,20 +1,25 @@
 const router = require('express').Router()
 const mysqlConnection = require('../database/database')
 
-router.get('/getstudents', (req, res) => {
+router.get('/students', (req, res) => {
     mysqlConnection.query(`
     SELECT student.id as studentId, student.name, student.surname, student.rut, student.genre, school.id as schoolId, school.name as school, school.number, course.level, course.letter
     FROM student
     INNER JOIN course ON student.course_id = course.id
     INNER JOIN school ON course.school_id = school.id;
-    
     `, (err, results) => {
         res.send(results)
     })
 })
 
-router.get('/getschools', (req, res) => {
+router.get('/schools', (req, res) => {
     mysqlConnection.query("SELECT * FROM school", (err, results) => {
+        res.send(results)
+    })
+})
+
+router.get('/instruments', (req, res) => {
+    mysqlConnection.query("SELECT * FROM instrument", (err, results) => {
         res.send(results)
     })
 })
--- src/routes/test.js
+++ src/routes/test.js
@@ -20,13 +20,11 @@ router.post('/', (req, res, next) => {
 
 
     if (data[0] == undefined) {
-        console.log("AGREGANDO 1 ELEMENTO")
         mysqlConnection.query(`INSERT INTO user (id, email) VALUES (${data['id']}, '${data['email']}')`, (err, res) => {
             console.log(res)
             if (err) throw err; 
         })
     } else {
-        console.log("AGREGANDO VARIOS ELEMENTOSSSSS")
         data.forEach(element => {
             
             mysqlConnection.query(`INSERT INTO user (id, email) VALUES (${element['id']}, '${element['email']}')`, (err, res) => {
