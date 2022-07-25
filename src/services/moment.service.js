9546ce0f2d753291bc7ab529a096f1b78b71e08b
k3yzen <alonso.anavalon@gmail.com>
2022-07-25 11:34:35 -0400

    moment service implemented

--- src/services/moment.service.js
+++ src/services/moment.service.js
@@ -1,13 +1,37 @@
 const mysqlConnection = require('../database/database')
-const mysql = require('mysql')
 
-exports.getMoments = () => {
-    const sql = `SELECT * FROM moment`;
+exports.getResults = (sql) => {
     return new Promise((resolve, reject) => {
         mysqlConnection.query(sql, (err, res) => {
+            if (err) throw err;
             const data = JSON.stringify(res)
             resolve (data);
         })
     })
+}
 
+exports.getMoments = () => {
+    const sql = `SELECT * FROM moment`;
+    return this.getResults(sql);
+}
+
+exports.getMoment = (id) => {
+    const sql = `SELECT * FROM moment WHERE moment.id = ${id}`;
+    return this.getResults(sql);
+}
+
+exports.createMoment = (studyId, begin, until) => {
+    const sql = `INSERT INTO moment (study_id, begin, until) VALUES (${studyId}, '${begin}', '${until}')`;
+    return this.getResults(sql);
+}
+
+exports.updateMoment = (momentId, begin, until) => {
+    const sql = `UPDATE moment SET begin='${begin}', until='${until}' WHERE moment.id = ${momentId}`;
+    return this.getResults(sql);
+    
+}
+
+exports.deleteMoment = (momentId) => {
+    const sql = `DELETE FROM moment WHERE moment.id = ${momentId}`;
+    return this.getResults(sql);
 }
