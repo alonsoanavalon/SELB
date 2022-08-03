36c5285f69282f9df752376d50cc15a89b95e626
k3yzen <alonso.anavalon@gmail.com>
2022-08-03 15:30:42 -0400

    devuelve completas y actualizadas, mejoras cuando no hay momento x fecha

--- src/controllers/instrumentControllers.js
+++ src/controllers/instrumentControllers.js
@@ -6,26 +6,39 @@ exports.saveDataFromInstrument = (req, res) => {
     mysqlConnection.query(`INSERT INTO test (test) VALUES ('${data.test}')`, (err, results, rows) => {
         if (err) throw err;
     })
-}   
+} 
+
+exports.getLastMoment = () => {
+    return new Promise( async (resolve, reject) => {
+        await mysqlConnection.query('SELECT id FROM moment ORDER BY id DESC LIMIT 1', (err, results) => {
+            if (err) throw err;
+            console.log(err, "ERROR!")
+            resolve(results)
+        })
+    })
+}
 
 exports.getMoment = (date) => { 
 
     console.log("fecha: ", date)
     return new Promise((resolve, reject) => {
-        mysqlConnection.query(`SELECT moment.id FROM moment WHERE moment.begin <= '${date}' AND moment.until >= '${date}'`, (err, results) => {
-            
-            if (results !== undefined) {
+        mysqlConnection.query(`SELECT moment.id FROM moment WHERE moment.begin <= '${date}' AND moment.until >= '${date}'`, async (err, results) => {
+            if (results !== undefined && results.length > 0) {
                 results = JSON.parse(JSON.stringify(results))
                 id = results[0]['id']
                 if (id) {
                     resolve(id) 
                 }
                 else {
-                    throw new Error('No existe el momento')
+                    console.log('No existe el momento, consiguiendo last moment 1')
+                    id = await this.getLastMoment()
+                    resolve(JSON.parse(JSON.stringify(id))[0]['id'])
                 }
             } else {
-                console.error("No existe el momento")
-                resolve(2)
+                console.log("No existe el momento, consiguiendo last moment 2")
+                id = this.getLastMoment()
+                console.log(id, "id")
+                resolve(JSON.parse(JSON.stringify(id))[0]['id'])
 /*                 const today = new Date();
                 const untilDate = (today.getFullYear() + 1)+'/'+(today.getMonth()+1)+'/'+today.getDate();
                 
@@ -103,14 +116,14 @@ exports.getInstrumentList = (evaluationId, instrumentId) => {
     })
 }
 
-exports.saveInstrumentData = (infoObject, choicesObject, instrumentIndex) => {
+exports.saveInstrumentData = async (infoObject, choicesObject, instrumentIndex) => {
 
 
     let instrumentId = infoObject['instrument'];
     let studentId = infoObject['student_id']
     let userId = infoObject['user_id']
     let instrumentDate = infoObject['date']
-
+    let isCreated = undefined;
     let evaluationId;
     let newInstrumentId;
     let updateInstrument = true; 
@@ -119,8 +132,7 @@ exports.saveInstrumentData = (infoObject, choicesObject, instrumentIndex) => {
 
     return new Promise((resolve, reject) => {
 
-        console.log(`InstrumentId 
-        ${instrumentId}, StudentId ${studentId}, userId ${userId}, instrumentDate ${instrumentDate}`)
+        console.log(`InstrumentId ${instrumentId}, StudentId ${studentId}, userId ${userId}, instrumentDate ${instrumentDate}`)
 
         const today = new Date();
         const date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
@@ -130,7 +142,6 @@ exports.saveInstrumentData = (infoObject, choicesObject, instrumentIndex) => {
     
             evaluationId = await this.getEvaluation(res, studentId)
             
-    
             if (evaluationId === false) {
                 evaluationId = await this.createEvaluation(userId, studentId, res)
             } 
@@ -167,6 +178,7 @@ exports.saveInstrumentData = (infoObject, choicesObject, instrumentIndex) => {
             if (updateInstrument) {
                 
                 let counter = 0;
+                isCreated = false;
     
                 for (choice in choicesObject) {
                     counter += 1
@@ -178,7 +190,7 @@ exports.saveInstrumentData = (infoObject, choicesObject, instrumentIndex) => {
             } else {
     
                 let counter = 0;
-       
+                isCreated = true;
                 sql += `INSERT INTO choice (item_id, value, instrument_list_id) VALUES `
                 for (choice in choicesObject) {
                     counter += 1
@@ -196,16 +208,16 @@ exports.saveInstrumentData = (infoObject, choicesObject, instrumentIndex) => {
             }
     
     
-            resolve(mysqlConnection.query(sql, (err, res) => {
+            await mysqlConnection.query(sql, (err, res) => {
                 if(err){
                     console.log("Error en índice: ", instrumentIndex)
                     throw err;
                 } else {
-                    console.log(`Test N°${instrumentIndex} ingresado con exito`)
+                    console.log(`Test N°${instrumentIndex} ingresado con exito`)                
                 }
-                console.log(newInstrumentId)
+                resolve(isCreated)
                 
-            }))
+            })
     
             
 
--- src/routes/postData.js
+++ src/routes/postData.js
@@ -2,27 +2,51 @@ const router = require('express').Router()
 const instrumentControllers = require('../controllers/instrumentControllers')
 
 router.post('/newevaluation', async (req, res) => {
-    
     let allInstruments = req.body
-
+    let updatedCounter = 0;
+    let createdCounter = 0;
+    
 /*     allInstruments.forEach( instrument => {
         let [infoObject, choicesObject] = instrument
         instrumentControllers.saveInstrumentData(infoObject, choicesObject)
         
     }) */
 
-    console.log(`[                       INGRESARON ${allInstruments.length} test                        ]`)
     let instrumentIndex = 1;
     for (const instrument of allInstruments) {
-        console.log(`TEST N° ${instrumentIndex} ---------------------------------------------`)
+
         let [infoObject, choicesObject] = instrument
-        await instrumentControllers.saveInstrumentData(infoObject, choicesObject, instrumentIndex)
-        console.log(`FIN  N° ${instrumentIndex} **********************************************`)
+        let response = await instrumentControllers.saveInstrumentData(infoObject, choicesObject, instrumentIndex)
+        if (response === false) {
+            updatedCounter++
+        } else {
+            createdCounter++
+        }
+        
         instrumentIndex++
  
     }
-    console.log(`[                         FIN                                                           ]`)
-    
+    console.log(`Ingresaron ${allInstruments.length} Test: [Actualizados: ${updatedCounter}] [Creados: ${createdCounter}] `)
+
+    if (allInstruments.length !== updatedCounter + createdCounter) {
+        res.send({
+            instrumentsLength: `${allInstruments.length}`,
+            statusText: 'Los test no se han guardado con éxito',
+            htmlText: '<p>Intenta repetir este proceso o descargar tu <a href="/respaldo">respaldo</a> y comunicarte con el administrador</p>',
+            updatedCounter,
+            createdCounter,
+        }) 
+    } else {
+        res.send( {
+            instrumentsLength: `${allInstruments.length}`,
+            statusText: `Todos los test se han guardado con éxito `,
+            htmlText: '<p>Recuerda que la cantidad de test que se agregarán a tus "Test enviados" serán los <b>ingresados</b>, no aquellos <b>actualizados</b></p><br>En caso que existan inconsistencias, puedes descargar tu <a href="/respaldo">respaldo</a> y comunicarte con el administrador',
+            updatedCounter,
+            createdCounter,
+        
+        })
+    }
+
 
 })
 
