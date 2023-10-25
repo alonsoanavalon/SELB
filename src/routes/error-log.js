097cf65c2f3a63378f75f8e1b4bfec287f18fbf1
alonsoanavalon <alonso.anavalon@gmail.com>
2023-10-25 01:08:29 -0300

    error log agregado

--- src/routes/error-log.js
+++ src/routes/error-log.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const fs = require('fs');
 
 router.post('/', async (req, res) => {
-    const errorLog = req.body;
-    console.log(errorLog);
-    res.send(errorLog)
+    try {
+        const errorLog = req.body
+        const errorMessage = JSON.stringify(req.body.error);
+
+        // Agregar el error al archivo de registro
+        const timestamp = new Date().toISOString();
+        const logEntry = `${timestamp}: ${errorMessage}: ${JSON.stringify(errorLog)}\n`;
+        fs.appendFileSync('error.log', logEntry);
+    
+        res.status(200).send('Error registrado con éxito.');
+      } catch (err) {
+        console.error('Error al registrar el error:', err);
+        res.status(500).send('Error al registrar el error.');
+      }
 })
 module.exports = router
