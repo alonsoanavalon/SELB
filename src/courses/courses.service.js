f0294693587cb06cbb19612e777c1bb74b29ab65
Alonso Anavalon <alonso.anavalon@gmail.com>
2024-04-21 20:27:50 -0400

    returning course year

--- src/courses/courses.service.js
+++ src/courses/courses.service.js
@@ -3,7 +3,7 @@ const mysqlConnection = require('../database/database')
 exports.getAllCourses = () => {
     return new Promise( async (resolve, reject) => {
         try {
-            const sql = `SELECT  course.id, school.id as school_id, school.name, course.level, course.letter FROM course INNER JOIN school ON school.id = course.school_id order by id;`
+            const sql = `SELECT  course.id, school.id as school_id, school.name, course.level, course.letter, course.year FROM course INNER JOIN school ON school.id = course.school_id order by id;`
             await mysqlConnection.query(sql, async (err, results) => { 
                 resolve(results)
             })
